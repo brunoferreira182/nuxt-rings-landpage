8db186c65371df88fde85a03c0f7bb886db387c3
Bruno. 182 <brunoferreira1821995@gmail.com>
2024-07-22 14:41:40 -0300

    aoskdpa

---
+++ app.vue
@@ -0,0 +1,28 @@
+  <template>
+    <TresCanvas window-size >
+      <TresPerspectiveCamera :position="[0.1, 0.1, 0.1]" />
+      <OrbitControls/>
+      <Suspense>
+
+        <Ring></Ring>
+      </Suspense>
+      <!-- <TresMesh ref="sphereRef">
+
+        <TresSphereGeometry />
+        <TresMeshNormalMaterial/>
+      </TresMesh> -->
+      <TresGridHelper />
+    </TresCanvas>
+  </template>
+
+  <script  lang="ts" setup>
+    const sphereRef = ref()
+    const { onLoop } = useRenderLoop() 
+
+    onLoop(({elapsed}) => {
+      if(sphereRef.value){
+
+        sphereRef.value.position.y = Math.sin(elapsed)
+      }
+    })
+  </script>
---
+++ components/Ring.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Mesh, MeshBasicMaterial } from 'three';
+const {scene: model, nodes} = await useGLTF('./the_one_ring.glb')
+const ringTexture = await useTexture(['./OneRing_emissive.png'])
+const ringBakedMaterial = new MeshBasicMaterial({
+  map: ringTexture
+})
+ringTexture.flipY = false
+nodes.OneRing_OneRing_0.material = ringBakedMaterial
+console.log(nodes, 'opoakdpoak')
+</script>
+
+<template>
+  <primitive :object="nodes.OneRing_OneRing_0
+" />
+</template>
+
